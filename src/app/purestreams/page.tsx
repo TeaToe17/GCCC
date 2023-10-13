7f8afef4cecc582ae7573621b947389fd8ca1841
TeaToe17 <titobiloluwaa83@gmail.com>
2023-10-13 22:19:38 +0100

    gh

--- src/app/purestreams/page.tsx
+++ src/app/purestreams/page.tsx
@@ -2,13 +2,24 @@ import Header from '@/components/Header'
 import MinistryComp from '@/components/MinistryComp'
 
 import img1 from "@/../../public/pureStreamImg1.png";
+import img2 from "@/../../public/pureStreamImg2.png";
+import img3 from "@/../../public/pureStreamImg3.png";
+import img4 from "@/../../public/pureStreamImg4.png";
+import img5 from "@/../../public/pureStreamImg5.png";
+import img6 from "@/../../public/pureStreamImg6.png";
 
 const PureStreams = () => {
     return(
         <div>
             <Header />
-            <MinistryComp  
+            <MinistryComp 
+            name = "Pure Streams"
             image1 = {img1}
+            image2 = {img2}
+            image3 = {img3}
+            image4 = {img4}
+            image5 = {img5}
+            image6 = {img6}
             />
         </div>
     )
--- src/components/MinistryComp.tsx
+++ src/components/MinistryComp.tsx
@@ -6,7 +6,7 @@ const MinistryComp = (props) => {
     return (
       <div>
         <div className="flex flex-col items-center justify-center m-[40px] ">
-          <p className="font-bold text-[64px] ">Pure Streams</p>
+          <p className="font-bold text-[64px] "> {props.name} </p>
           <p className="mt-[20px] mb-[20px] text-center ">
             Experience the transformative power of worship and encounter God's{" "}
             <br />
@@ -25,17 +25,17 @@ const MinistryComp = (props) => {
             className=" w-[296px] h-[412px] m-[10px] "
           />
           <Image
-            src={props.image1}
+            src={props.image2}
             alt="img"
             className=" w-[296px] h-[412px] m-[10px] "
           />
           <Image
-            src={props.image1}
+            src={props.image3}
             alt="img"
             className=" w-[296px] h-[412px] m-[10px] "
           />
           <Image
-            src={props.image1}
+            src={props.image4}
             alt="img"
             className=" w-[296px] h-[412px] m-[10px] "
           />
@@ -59,8 +59,8 @@ const MinistryComp = (props) => {
             <img src="polygon 2.png" alt="" />
           </div>
           <div className="flex overflow-scroll :web ">
-            <PureStreams image1={props.image1} />
-            <PureStreams image1={props.image1} />
+            <PureStreams image1={props.image5} />
+            <PureStreams image1={props.image6} />
             <PureStreams image1={props.image1} />
           </div>
           <div className="bg-[url('/purestreampic.png')] w-full h-[500px] mt-[50px] text-white flex flex-col  justify-center p-[30px] ">
